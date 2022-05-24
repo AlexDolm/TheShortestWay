af4ea3967e8bf981046dc6b693f4adaa1ea9cb4a
AlexDolm <sancho0_0@mail.ru>
2022-05-25 02:00:30 +0300

    yandex map

---
+++ TheShortestWay/YandexMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  YandexMapViewController.swift
+//  TheShortestWay
+//
+//  Created by mac on 25.05.2022.
+//
+
+import UIKit
+
+class YandexMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
